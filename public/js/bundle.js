b169e92d2cd7b6863bf2c5ef4fd18fd8d037fa7f
Raph <raph4sure007@gmail.com>
2024-08-11 13:50:48 +0100

    merging conflict

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12891,7 +12891,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44725" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];

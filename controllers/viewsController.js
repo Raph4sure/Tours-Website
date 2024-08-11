96f243ba2409c39b57dfec13789fda88a8925358
Raph <raph4sure007@gmail.com>
2024-08-11 11:04:11 +0100

    implementing Logging in Users

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,7 +11,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async(req, res) => {
+exports.getTour = catchAsync(async(req, res, next) => {
   // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -22,3 +22,10 @@ exports.getTour = catchAsync(async(req, res) => {
     tour
   });
 });
+
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  })
+}

de07ceb617730319600f396bc9e267b5aeac1e33
Raph <raph4sure007@gmail.com>
2024-08-15 23:21:53 +0100

    Cleaning Up 2 and doing final building

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -66,7 +66,7 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
       }
     }
   ]);
-  console.log(stats);
+  // console.log(stats);
 
   await Tour.findByIdAndUpdate(tourId, {
     ratingsQuantity: stats[0].nRating,
@@ -82,7 +82,7 @@ reviewSchema.post('save', function() {
 // The Regex Expression /^findOneAnd/ works for findByIdAndUpdate and findByIdAndDelete
 reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
-  console.log(this.r);
+  // console.log(this.r);
   next();
 });
 
